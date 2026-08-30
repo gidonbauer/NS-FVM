a7154db742d5b081f520b634d9793ed68ebd7aea
gidon.bauer <g.bauer@itv.rwth-aachen.de>
2026-08-30 22:02:44 +0200

    Let Claude implement a very naive Gauss-Seidel solver
    TODO: Maybe add a multigrid solver

--- test/test.mk
+++ test/test.mk
@@ -1,4 +1,5 @@
-TESTS = Taylor-Green-8 Taylor-Green-16 Taylor-Green-64 Taylor-Green-100
+TESTS = Taylor-Green-8 Taylor-Green-16 Taylor-Green-64 Taylor-Green-100 \
+        Gauss-Seidel-32 Gauss-Seidel-64 Gauss-Seidel-128
 
 # ------------------------------------------------------------------------------
 strip_digits = ${subst 9,,${subst 8,,${subst 7,,${subst 6,,${subst 5,,${subst 4,,${subst 3,,${subst 2,,${subst 1,,${subst 0,,$1}}}}}}}}}}
